51a916d1d39096b1883892f4eee557b0390efe30
Simobini74 <17884614@studenti.unipi.it>
2021-09-01 10:16:50 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,23 @@ RichiediChiave =async (key:string=null)=>{
   this.RichiediChiave(k); } 
   else{
     const datas = await this.postservice.getData(key, data => JSON.parse(data));
+    if(!datas.error) {
+      this.key = key;
+      if(Object.keys(datas).length > 0) {
+        let data = datas.filter(item => item.importante == 1);
+        for(var item of data) {
+          let post = new Post['pluto'];
+          post.importante = item.importante;
+          post.titolo = item.titolo;
+          post.contenuto = item.contenuto;
+          this.postit.push(post);
+
+          
+        }
+      }
+    } else {
+      alert("Chiave non valida");
+    }
 
     
     }
