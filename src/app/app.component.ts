e25e77932a861853e7368fdfa8b07007a92717ba
Simobini74 <17884614@studenti.unipi.it>
2021-09-01 07:19:35 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,15 @@ export class AppComponent  {
   name = 'Progetto SSW';
   title = 'Applicazione per Postit';
   key : string;
+  postit= Array<Post>;
+
+  constructor(private postservice: PostserviceService) {
+    this.postit = new Array<Post>();
+  }
+
+ inviaChiave = async (key: string = null) =>{
+  const k = await this.postservice.requestKey();
+  this.inviaChiave(k);
+
+ }
 }
