ad7f21bdcf78b040fde0ae3c2d147c0ed9aa4f10
Simobini74 <17884614@studenti.unipi.it>
2021-09-01 11:08:00 +0200

    1 file modified, 1 added

--- src/app/postit/postit.component.ts
+++ src/app/postit/postit.component.ts
@@ -2,17 +2,15 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { PostserviceService } from '../postservice.service';
 import { Post } from '../app.component';
 
-
 @Component({
   selector: 'app-postit',
   templateUrl: './postit.component.html',
   styleUrls: ['./postit.component.css']
 })
 
-
 export class PostitComponent implements OnInit {
   @Input() key: string;  
-  @Input() postit: Array<Post>
+  @Input() postit: Array<Post>;
   creazione_post: boolean;
   post_visualizzato: number;
   ngOnInit(): void { }
@@ -21,12 +19,17 @@ export class PostitComponent implements OnInit {
     this.post_visualizzato = -1;
    }
   creaPost = async (titolo: string, contenuto: string, importante: string) =>{
-    if(titolo.trim().length !== 0 && contenuto.trim().length !== 0) {     
-     
-    await this.postservice.sendData(this.key, this.postit);
-
-
-
+    if(titolo.trim().length !== 0 && contenuto.trim().length !== 0) {          
+      var post = new Post();
+      post.importante = Number(importante);
+      post.titolo = titolo;
+      post.contenuto = contenuto;
+      this.postit.push(post);
+      await this.postservice.sendData(this.key, this.postit);
+      this.creazione_post = false;
+    } else {
+      alert("Non puoi creare un post vuoto");
+    }
 
   }}
 
