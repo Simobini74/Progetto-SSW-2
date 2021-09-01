fa4c6ab6d1cf96ffa99c4ce43ff557169e2eac46
Simobini74 <17884614@studenti.unipi.it>
2021-09-01 07:07:39 +0200

    1 file added

---
+++ src/app/postservice.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PostserviceService {
+  URLbase : string;
+
+  constructor() { this.URLbase = "https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook";}
+
+  requestKey = () => {
+    return fetch(this.URLbase + "/new", {method: "POST"})
+    .then(response => response.json(), error => alert(error));
+  }
+
+}
