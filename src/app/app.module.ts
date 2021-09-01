bf428f666d4c6f4a1385fddf21719dce99c956b2
Simobini74 <17884614@studenti.unipi.it>
2021-09-01 10:47:18 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PostitComponent } from './postit/postit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, PostitComponent],
   bootstrap:    [ AppComponent ],
   providers: [] 
 
--- src/app/postit/postit.component.ts
+++ src/app/postit/postit.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { PostserviceService } from '../postservice.service';
-import { Post } from '../app.component';
+
 
 @Component({
   selector: 'app-postit',
@@ -8,19 +8,26 @@ import { Post } from '../app.component';
   styleUrls: ['./postit.component.css']
 })
 export class PostitComponent implements OnInit {
-  @Input() key: string;
+  @Input() key: string;  
   @Input() postit: Array<Post>;
-
-  constructor(private postservice: PostserviceService) { }
+  creazione_post: boolean;
+  post_visualizzato: number;
+  ngOnInit(): void { }
+  constructor(private postservice: PostserviceService) {
+    this.creazione_post = false;
+    this.post_visualizzato = -1;
+   }
   creaPost = async (titolo: string, contenuto: string, importante: string) =>{
+    if(titolo.trim().length !== 0 && contenuto.trim().length !== 0) {     
+     
     await this.postservice.sendData(this.key, this.postit);
 
 
 
 
-  }
+  }}
 
-  ngOnInit() {
-  }
+ 
+  
 
 }
