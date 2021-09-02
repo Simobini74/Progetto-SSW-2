d4e73250503dc2219cae224f14ea57cdebcbb80d
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 06:53:03 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 import { PostserviceService } from './postservice.service';
 
-export class Post {
+export class Postit {
   titolo : string;
   contenuto: string;
   importante: boolean;
@@ -16,40 +16,22 @@ export class Post {
 export class AppComponent  {
   name = 'Progetto SSW';
   title = 'Applicazione per Postit';
-  key : string;
-  postit: Array<Post>;
-  constructor(private postservice: PostserviceService) {
-    this.postit = new Array<Post>();
+  selezione: Postit= new Postit();
+  key : '';
+  postalvato: Array<Postit>={};
+  flag: boolean=false;
+  constructor(private postservice: PostserviceService) {   
   }
-RichiediChiave =async (key:string=null)=>{
-  if (key===null){
-  const k =await this.postservice.requestKey();
-  this.RichiediChiave(k); } 
-  else{
-    const datas = await this.postservice.getData(key, data => JSON.parse(data));
-    if(!datas.error) {
-      this.key = key;
-      if(Object.keys(datas).length > 0) {
-        let data = datas.filter(item => item.importante == 1);
-        for(var item of data) {
-          let post = new Post['pluto'];
-          post.importante = item.importante;
-          post.titolo = item.titolo;
-          post.contenuto = item.contenuto;
-          this.postit.push(post);
+  MostraPost(id: Postit) {
+    this.selezione.titolo = id.titolo;
+    this.selezione.contenuto = id.contenuto;
+  }
+}
+
 
           
-        }
-      }
-    } else {
-      alert("Chiave non valida");
-    }
-
-    
-    }
+   
 
 
   
- }
- 
-}
+
