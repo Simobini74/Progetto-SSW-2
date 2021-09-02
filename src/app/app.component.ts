802b7b16a302b5c85132c7ac03d1295acbd76c44
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 06:59:47 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,21 @@ export class AppComponent  {
   title = 'Applicazione per Postit';
   selezione: Postit= new Postit();
   key : '';
-  postalvato: Array<Postit>={};
+  postsalvato: Array<Postit>=[];
   flag: boolean=false;
   constructor(private postservice: PostserviceService) {   
   }
   MostraPost(id: Postit) {
     this.selezione.titolo = id.titolo;
     this.selezione.contenuto = id.contenuto;
+  }
+  addPost(newPostit: Postit) {
+    this.postservice.apiKEY = this.key;
+    this.postsalvato.push(newPostit);
+    let newmsg: string = JSON.stringify(this.postsalvato);
+    this.postservice
+      .postData(newmsg)
+      .then(response => response.json(), error => alert(error));
   }
 }
 
--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -1,29 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable(
  { providedIn: 'root'}
 )
 export class PostserviceService {
-  baseURL : string;
+  apiKEY: string = '';
+  apiURL: string =
+    'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
 
-  constructor() { this.baseURL = "https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook";}
+  constructor(private service: HttpClient) { }
 
-  requestKey = () => {
-    return fetch(this.baseURL + "/new", {method: "POST"})
-    .then(response => response.json(), error => alert(error))
-        
- 
-  }
-  getData = (key: string, callback: any) => {
-    return fetch(this.baseURL + "/get?key=" + key)
-    .then(response => response.json(), error => alert(error))    
-    .then(data => callback(data));    
-  
-  }
-  sendData = (key: string, msg: {}) => {
-    return fetch(this.baseURL + "/post?key=" + key + "&msg=" + JSON.stringify(msg), {method: "POST"})
-    .then(response => response.json(), error => alert(error));
-     }
  
 
 
